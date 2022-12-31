a816e21017ea6c4d8bf4540883c3ab2c25859a8a
GalangBienew <galangriziq12@gmail.com>
2023-01-01 02:07:05 +0700

    done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import NavbarG from "./Component/pages/Navbar";
 import "../src/index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
-// import Particles from "react-tsparticles";
-// import ParticlesBg from "./Component/particles/particles";
-// import Particles from "react-tsparticles";
-// import Particlesbackground from "./config/particelbackground";
-// import Particlesbackground from "./config/particelbackground";
 const About = lazy(() => import("./Component/About/About"));
 const Headerthis = lazy(() => import("./Component/Headerfirst/Headerthis"));
 const Contact = lazy(() => import("./Component/Contact/Contact"));
@@ -20,7 +15,7 @@ function App() {
         <NavbarG />
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route exact path="/" element={<About />} />
+            <Route path="/" element={<About />} />
             <Route path="/Headerthis" element={<Headerthis />} />
             <Route path="/Contactus" element={<Contact />} />
             <Route path="/project" element={<Project />} />
